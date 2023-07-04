e97a03ba6f89feee9caa39a87c35f77c8cf25888
Hariadithyan <52594178+Hariadithyan@users.noreply.github.com>
2023-07-04 17:11:53 +0530

    Add files via upload

--- fb.js
+++ fb.js
@@ -5,18 +5,25 @@ var play1={
 
  var play2={
     p2_count:0,
-    val:[]
+    val:[],
 };
 var count=0;
 console.log("Strike");
 p1_round=document.getElementById("team1").children;
 p2_round=document.getElementById("team2").children;
 document.getElementById("strike").onclick=function(){
+   
 
     var r1=Math.floor(Math.random()*100+1);
     var r2=Math.floor(Math.random()*100+1);
-
-   if(count==5)
+    console.log(r1);
+    console.log(r2);
+    if(count==5)
+    {
+        document.getElementById("strike").textContent="Who Wins?";
+    }
+  
+   else if(count==6)
    {
     if(play1.p1_count<play2.p2_count){
     document.getElementById("strike").textContent="Match WIN by team MANCHESTER UNITED";
@@ -28,16 +35,18 @@ document.getElementById("strike").onclick=function(){
     else{
         document.getElementById("strike").textContent="Match DRAW";
     }
-}
+   }
 else if(count<5){
     if(r1%2==0)
     {
+        
         play1.p1_count++;
         // play1.val.push(r1);
         document.getElementById("t1score").textContent=play1.p1_count;
     }
     if(r2%2==0)
     {
+        // console.log(r2);
         play2.p2_count++;
         // play2.val.push(r2);
         document.getElementById("t2score").textContent=play2.p2_count;
@@ -49,26 +58,33 @@ else if(count<5){
 count++;
 }
 function round(){
+    c=0,d=0;
     play1.val.forEach((a,b)=>{
+        console.log("value of p1",++c," ",a);
         if(a%2==0){
+          
             p1_round[b].style.backgroundColor="green";
         }
         else
         {
+            // console.log("value of p1",b);
             p1_round[b].style.backgroundColor="red";
         }
     });
     play2.val.forEach((a,b)=>{
+        console.log("value of p2",++d," ",a);
         if(a%2==0){
+            
             p2_round[b].style.backgroundColor="green";
         }
         else
         {
+            // console.log("value of p2",b);
             p2_round[b].style.backgroundColor="red";
         }
     });
+    console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 
 }
-
 
 
